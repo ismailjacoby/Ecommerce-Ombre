89d4329c115dacac9f7a2676f0ff17deac1c81eb
Ismail <ismail.jacoby@gmail.com>
2024-11-21 18:35:50 +0100

    Initial commit: Add Spring backend and Angular frontend +  TailwindCss.

---
+++ EcommerceBackend/src/test/java/com/ismailjacoby/ecommercebackend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
